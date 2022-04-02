2c5b47d1283a90444b2c9dc2d28b633e1c70a07a
vargas.flavio@gmail.com <vargas.flavio@gmail.com>
2022-04-02 08:33:14 -0300

    first deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req,res) => {
-    res.send("Root da API");
+    res.send("Rota raíz da API");
 });
 
 app.listen(port, () => {
